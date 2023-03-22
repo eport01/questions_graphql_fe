24b670dd02a508092d60e1ef630ae16c9a5d88f9
Emily Port <emilylport@gmail.com>
2023-03-21 19:11:18 -0600

    add login component and tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import BQuestionsList from './components/BQuestionsList';
 import {Route, Routes} from "react-router-dom";
+import Login from './components/Login'
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route strict exact path="/" element={<BQuestionsList/>}/>
+        <Route strict exact path="/" element={<Login/>}/>
+        <Route strict exact path="/bquestions" element={<BQuestionsList/>}/>
+
       </Routes>
     </div>
   );
